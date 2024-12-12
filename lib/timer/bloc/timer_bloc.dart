3083838b0f15551ab3e138f5ae041de81ec87e10
Luci4aayush <74036083+aayushxadhikari@users.noreply.github.com>
2024-12-12 12:47:59 +0545

    Update timer_bloc.dart

--- lib/timer/bloc/timer_bloc.dart
+++ lib/timer/bloc/timer_bloc.dart
@@ -7,7 +7,7 @@ import '../../ticker.dart';
 
 part 'timer_event.dart';
 part 'timer_state.dart';
-
+//
 class TimerBloc extends Bloc<TimerEvent, TimerState> {
   final Ticker _ticker;
   static const int _duration = 60;
